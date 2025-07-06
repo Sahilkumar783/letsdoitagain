e61f4195e007cd2e7298849a12899a99e182682b
Sahilkumar783 <sahil871012@gmail.com>
2025-07-06 17:30:38 +0530

    bastion subnet added

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "azurerm_subnet" "example" {
 }
 
 resource "azurerm_subnet" "example" {
-  name                 = "BackendSubnet"
+  name                 = "BastionSubnet"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
